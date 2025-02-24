82aab47ff3f592265ee2a40be4902fe26ba83851
Reva <111722589+rssathe08311@users.noreply.github.com>
2025-02-24 16:45:39 -0500

    reverted code

--- workshop_4_sathe/script.js
+++ workshop_4_sathe/script.js
@@ -11,9 +11,15 @@ const loadPlaces = () => {
     places.forEach(place => {
         const entity = document.createElement("a-entity");
 
+
+        const shape = document.createElement("a-sphere");
+        shape.setAttribute("color", "blue");
+        entity.appendChild(shape);
+
+
         entity.setAttribute("gps-entity-place", `latitude: ${place.latitude}; longitude: ${place.longitude}`);
-        entity.setAttribute("geometry", "primitive: sphere");
-        entity.setAttribute("material", "color: blue");
+        //entity.setAttribute("radius", "1");
+        //entity.setAttribute("color", "blue");
 
         const text = document.createElement("a-text");
         text.setAttribute("value", place.name);
@@ -28,37 +34,3 @@ const loadPlaces = () => {
 };
 
 window.onload = loadPlaces;
-
-//// For testing puroposes, you may need to change these places to something closer to you
-//const places = [
-//    { name: "Rochester Abandoned Subway", latitude: 43.154722, longitude: -77.609722 },
-//    { name: "Washington Square Park", latitude: 43.1534, longitude: -77.6053 },
-//    { name: "Rochester Contemporary Art Center", latitude: 43.156619, longitude: -77.600730 }
-//];
-//
-//const loadPlaces = () => {
-//    const scene = document.querySelector("a-scene");
-//
-//    places.forEach(place => {
-//        const shape = document.createElement("a-sphere");
-//        shape.setAttribute("color", "blue");
-//        entity.appendChild(shape);
-//
-//        const entity = document.createElement("a-entity");
-//        entity.setAttribute("gps-entity-place", `latitude: ${place.latitude}; longitude: ${place.longitude}`);
-//        //entity.setAttribute("radius", "1");
-//        //entity.setAttribute("color", "blue");
-//
-//        const text = document.createElement("a-text");
-//        text.setAttribute("value", place.name);
-//        text.setAttribute("align", "center");
-//        text.setAttribute("position", "0 2 0");
-//        entity.appendChild(text);
-//
-//        entity.addEventListener("click", () => alert(`You clicked on: ${place.name}`));
-//
-//        scene.appendChild(entity);
-//    });
-//};
-//
-//window.onload = loadPlaces;
